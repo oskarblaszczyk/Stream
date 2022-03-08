247fd7a8a1371ee1101e3cf86c812f80649869d6
blasoska <blasoska@WRO5-PF364QY9.ant.amazon.com>
2022-03-08 16:08:50 +0100

    RowerDzieciecy edited

--- Obiektowosc2/src/rower/RowerDzieciecy.java
+++ Obiektowosc2/src/rower/RowerDzieciecy.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class RowerDzieciecy extends Rower {
 
 	private boolean kolaBoczne;
-	private static List<RowerDzieciecy> ekstensja = new ArrayList<>();
+	//private static List<RowerDzieciecy> ekstensja = new ArrayList<>();
 
 	public RowerDzieciecy(String marka, String model, int rokProdukcji, int nrSeryjny, boolean kolaBoczne) {
 		super(marka, model, rokProdukcji, nrSeryjny);
 		this.kolaBoczne = kolaBoczne;
 
-		ekstensja.add(this);
+		//ekstensja.add(this);
 		
 		
 	}
